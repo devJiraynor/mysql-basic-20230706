4a15217098b144bb89aaddc207a47f90877fea1e
devJiraynor <jiraynor@naver.com>
2023-08-02 12:19:34 +0900

    2023.08.02. 12:20

--- SQL-Advanced.sql
+++ SQL-Advanced.sql
@@ -27,7 +27,49 @@ CREATE TABLE employee (
     FOREIGN KEY (department_name) REFERENCES department (department_name)
 );
 
+INSERT INTO department 
+VALUES ('기획부', '051-111-1111', '부산광역시');
+INSERT INTO department 
+VALUES ('영업부', '051-222-1111', '부산광역시');
+INSERT INTO department 
+VALUES ('연구소', '051-333-1111', '부산광역시');
+INSERT INTO department 
+VALUES ('인사부', '051-444-1111', '부산광역시');
+INSERT INTO department 
+VALUES ('개발부', '051-555-1111', '부산광역시');
 
+INSERT INTO employee 
+VALUES ('3015', '양희정', '부산광역시', '인사부');
+INSERT INTO employee 
+VALUES ('3032', '여상길', '부산광역시', '연구소');
+INSERT INTO employee 
+VALUES ('3076', '김경훈', '부산광역시', '영업부');
+INSERT INTO employee 
+VALUES ('1687', '조창석', '부산광역시', '개발부');
+INSERT INTO employee 
+VALUES ('3103', '유정봉', '부산광역시', '기획부');
+INSERT INTO employee 
+VALUES ('3110', '이수광', '부산광역시', '인사부');
+INSERT INTO employee 
+VALUES ('3133', '박진우', '부산광역시', '연구소');
+INSERT INTO employee 
+VALUES ('3138', '이수경', '부산광역시', '기획부');
+INSERT INTO employee 
+VALUES ('3141', '이지훈', '부산광역시', '영업부');
+INSERT INTO employee 
+VALUES ('3151', '최지선', '부산광역시', '개발부');
+INSERT INTO employee 
+VALUES ('2894', '정아린', '부산광역시', null);
+INSERT INTO employee 
+VALUES ('2939', '김정민', '부산광역시', '연구소');
+INSERT INTO employee 
+VALUES ('2375', '김유정', '부산광역시', '기획부');
+INSERT INTO employee 
+VALUES ('2861', '정이제', '부산광역시', '개발부');
+INSERT INTO employee 
+VALUES ('3263', '이소진', '부산광역시', null);
+INSERT INTO employee 
+VALUES ('0000', '서지훈', '부산광역시', null);
 
 
 
